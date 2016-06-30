e71181a399b63f84af8592994c591e225f0de6a9
Julien Esposito <julien.esposito@gmail.com>
2016-06-29 18:41:09 -0700

    Chapter 01 - Will Collaboration
    Chapter 01 - Will Collaboration

---
+++ mod07_2016_prg_451_Ch1/src/mod07_2016_prg_451_ch1/Mod07_2016_prg_451_Ch1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mod07_2016_prg_451_ch1;
+
+/**
+ *
+ * @author djulzz
+ */
+public class Mod07_2016_prg_451_Ch1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
